27ddc004bda5e1f15d79f1ffa196fa84620f5ce5
Davíd Roggér <davidrogger.a@gmail.com>
2022-09-13 13:23:55 -0300

    feat: add games display

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
-import { View, Image } from 'react-native';
+import { View, Image, FlatList } from 'react-native';
 
 import logoImg from '../../assets/logo-nlw-esports.png';
+import { GAMES } from '../../utils/games';
+import { GameCard } from '../../components/GameCard';
+import { Heading } from '../../components/Heading';
 
 import { styles } from './styles';
 
@@ -11,6 +13,24 @@ export function Home() {
       <Image 
         source={logoImg}
         style={styles.logo}
+      />
+
+      <Heading 
+        title="Encontre seu duo!"
+        subtitle="Selecione o game que deseja jogar..."
+      />
+
+      <FlatList 
+        data={ GAMES }
+        keyExtractor={game => game.id}
+        renderItem= {({ item:game }) => (
+          <GameCard 
+            data={ game }
+          />
+        )}
+        showsHorizontalScrollIndicator={ false }
+        horizontal
+        contentContainerStyle={ styles.contentList }
       />
     </View>
   );
