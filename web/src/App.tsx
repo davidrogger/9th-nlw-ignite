e178fd737d921ad0a2585ad278f86a76b41d21fd
Davíd Roggér <davidrogger.a@gmail.com>
2022-09-13 09:44:17 -0300

    feat: add initial title

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,15 @@
 import './styles/main.css';
+import logoImg from './assets/nlw-logo.svg';
+import games from '../public/tmpGamesImg/games';
 
 function App() {
   return (
-    <div>
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={logoImg} />
+
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu <span className='text-transparent bg-nlw-gradient bg-clip-text'>duo</span> está aqui
+      </h1>
     </div>
   );
 }
