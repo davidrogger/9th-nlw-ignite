0f74d599b9b7ac13f2a572b15f63c03996ac474d
Davíd Roggér <davidrogger.a@gmail.com>
2022-09-15 12:07:23 -0300

    feat: add cors
    library responsable to give access permition into the api

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import adRoute from './routes/ad.route';
 import gameRoute from './routes/game.route';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 3333;
 
 app.use(express.json());
+app.use(cors()); // permitir o acesso do frontend
 
 app.use('/games', gameRoute);
 app.use('/ads', adRoute)
